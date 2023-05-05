8172e3e5c884930c446ddaa46c10b68ab3abe4f9
Gaston Mitoire <gastonmm@gmail.com>
2023-05-05 08:13:17 -0300

    feat: header styles init

--- app/root.tsx
+++ app/root.tsx
@@ -46,7 +46,7 @@ function Document({ children, title }: PropsWithChildren<{ title?: string }>) {
         {title ? <title>{title}</title> : null}
         <Links />
       </head>
-      <body className="dark:bg-slate-800">
+      <body className="dark:bg-neutral-800">
         {children}
         <Scripts />
         <LiveReload />
@@ -57,8 +57,6 @@ function Document({ children, title }: PropsWithChildren<{ title?: string }>) {
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(true);
-
-  console.log(darkMode);
 
   const toggleDarkMode = () => {
     const newValue = !darkMode;
@@ -75,14 +73,44 @@ export default function App() {
 
   return (
     <Document>
-      <header className="bg-white dark:bg-gray-800">
-        <nav className="container mx-auto flex items-center justify-between">
-          <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
-            My Website
+      <header className="py-3">
+        <nav className="container mx-auto p-3 flex items-center justify-between bg-neutral-100 dark:bg-neutral-900">
+          <h1 className="text-2xl font-bold text-neutral-900 dark:text-neutral-100">
+            MyBlog
           </h1>
           <div className="flex items-center space-x-4">
             <button onClick={toggleDarkMode}>
-              {darkMode ? "Light" : "Dark"} Mode
+              {darkMode ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 stroke-yellow-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 stroke-blue-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                  />
+                </svg>
+              )}
             </button>
           </div>
         </nav>
