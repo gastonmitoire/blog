585480a7bd4c85590363026ba38383a470e31165
Gaston Mitoire <gastonmm@gmail.com>
2023-05-07 10:22:54 -0300

    feat: single post styles init

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -8,12 +8,19 @@ export function PostDisplay({
 }: {
   canDelete?: boolean;
   isOwner: boolean;
-  post: Pick<Post, "slug" | "title" | "image" | "secondaryImage">;
+  post: Pick<Post, "slug" | "title" | "image" | "secondaryImage" | "markdown">;
 }) {
   return (
-    <div>
+    <div className="flex flex-col gap-3 items-center bg-neutral-100 dark:bg-neutral-900">
       <img src={post?.image!} alt="" className="w-full" />
-      <p>{post.title}</p>
+      <h5 className="text-lg uppercase font-medium pt-5">TypeScript</h5>
+      <h2 className="text-3xl font-medium">{post.title}</h2>
+      <div
+        className="bg-red-300"
+        dangerouslySetInnerHTML={{
+          __html: post.markdown,
+        }}
+      />
       <Link to=".">"{post.slug}" Permalink</Link>
       {isOwner ? (
         <Form method="post">
