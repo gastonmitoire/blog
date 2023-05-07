913c65060688ff07d414ffe2395ae4e373fd990d
Gaston Mitoire <gastonmm@gmail.com>
2023-05-07 10:05:27 -0300

    feat: sidebar-list styles

--- app/components/accordion.tsx
+++ app/components/accordion.tsx
@@ -14,12 +14,12 @@ export const Accordion = ({
   const [isExpanded, setIsExpanded] = useState(defaultExpanded);
 
   return (
-    <div className="flex-0 flex flex-col bg-neutral-200 dark:bg-neutral-800">
+    <div className="flex-0 flex flex-col bg-neutral-200 dark:bg-neutral-800 shadow">
       <button
         onClick={() => setIsExpanded(!isExpanded)}
         className={`${
           isExpanded ? "rounded-t-md" : "rounded-md"
-        } flex items-center justify-between w-full px-3 py-2 text-sm font-medium text-left text-neutral-700 bg-white dark:bg-neutral-900 dark:text-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75`}
+        } flex items-center justify-between w-full px-3 py-2 font-medium text-left text-neutral-700 bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75`}
       >
         <span>{title}</span>
         <svg
@@ -40,7 +40,7 @@ export const Accordion = ({
       <div
         className={`${
           isExpanded ? "block" : "hidden"
-        } bg-neutral-900 rounded-b-md`}
+        } bg-neutral-200 dark:bg-neutral-800 rounded-b-md px-3`}
       >
         {children}
       </div>
--- app/components/sidebar_list.tsx
+++ app/components/sidebar_list.tsx
@@ -11,12 +11,14 @@ export const SidebarList: React.FC<SidebarListProps> = ({ title, items }) => {
       <div className="flex flex-col gap-2">
         <ul>
           {items.map((item, index) => (
-            <li key={index}>
+            <li key={index} className="group">
               <a
                 href="#"
-                className="flex items-center justify-between px-3 py-2 text-sm font-medium text-left text-neutral-700 bg-white dark:bg-neutral-900 dark:text-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75"
+                className="flex items-center justify-between px-3 py-2 text-sm font-medium text-left focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75"
               >
-                <span>{item.name}</span>
+                <span className="text-neutral-700 dark:text-neutral-200 group-hover:text-blue-500">
+                  {item.name}
+                </span>
                 {item.count && (
                   <span className="text-neutral-500">{item.count}</span>
                 )}
