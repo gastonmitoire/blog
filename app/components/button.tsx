d2076aca9ba97d8ee05569f7774f29f2a78686a3
Gaston Mitoire <gastonmm@gmail.com>
2023-05-07 06:00:00 -0300

    feat: button acepts icon

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -2,18 +2,27 @@ import React, { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   baseClassName?: string;
+  icon?: React.ReactNode;
 };
 
 export const Button: React.FC<ButtonProps> = ({
-  baseClassName = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
   children,
+  icon,
+  baseClassName = "w-full bg-blue-500 hover:bg-blue-700 text-neutral-200 uppercase py-2 px-4 rounded",
   ...rest
 }) => {
   const classNames = `${baseClassName} ${rest.className || ""}`;
 
   return (
     <button className={classNames} {...rest}>
-      {children}
+      {icon ? (
+        <span className="flex justify-center items-center space-x-2">
+          {icon}
+          {children}
+        </span>
+      ) : (
+        children
+      )}
     </button>
   );
 };
